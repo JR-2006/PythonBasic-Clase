b977ff09affacff6038247e9a23065bdfb81bff6
JohanRamirez1618 <johanramirez1618@gmail.com>
2024-09-05 14:15:53 -0500

    En el siguiente codigo se encuentra los input realizadores en clase

---
+++ Practica.py
@@ -0,0 +1,16 @@
+print ("¡Hola Mundo!")
+
+"""
+Nombre: Johan Yesid Ramirez Torres
+Materia:Programacion 1
+Correo: johanramirez124@unisangil.edu.co"""
+
+nombre = input("Digite su nombre completo: ")
+cedula = int(input("Digite su T.I o su C.C: "))
+estatura = float(input("Digite su estatura: "))
+estado = bool(input("Digite su estado civi: "))
+
+print(f"Su nombre es: {nombre}")
+print(f"Su cedula es: {cedula}")
+print(f"Su estatura es: {estatura}m")
+print(f"Su estado civil es: {estado}")
