40f31ac1d7d54d1a4f6459f4447fd01272a0e09c
JohanRamirez1618 <johanramirez1618@gmail.com>
2024-09-05 14:13:43 -0500

    Se realiza un nuevo repositorio por fallas de anteriores, donde en el presente repositorio se encuentra los ejercicios echos en clase

---
+++ OperadoresAritmeticos.py
@@ -0,0 +1,31 @@
+#Importar libreria
+import math 
+#Ejemplo 1
+ejemplo1 = 5*(2+5)
+#Imprimir primera variable
+print(f"El resultado de la primera opereacion es: {ejemplo1}")
+
+#Ejemplo2
+ejemplo2 = (4+3)-(3*2)+1
+#Imprimir segunda variable
+print(f"El resultado de la segunda operacion es: {ejemplo2}")
+
+#Ejemplo3
+ejemplo3 = 2*2+5*(9/3)
+#Imprimir tercera variable
+print(f"El resultado de la tercera operacion es: {ejemplo3}")
+
+#Ejemplo4
+ejemplo4 = 7+3*(1+5*(2*(9-7)+4)+2)
+#Imprimir cuarta operacion
+print(f"El resultado de la cuarta operacion es: {ejemplo4}")
+
+#Ejemplo5
+ejemplo5 = 15/3*(7+(68-15*33+((45**2)/5)/3)/2)+19
+#Imprimir quinta operacion
+print(f"El resultado de la quinta operacion es: {ejemplo5}")
+
+#Ejemplo6
+ejemplo6 = (16/4*((((4+8*2)/4-(3**2)+ math.sqrt(4) )/2)* math.sqrt(25) )-3)+16
+#Imprimir sexta operacion
+print(f"El resultado de la sexta operacion es: {ejemplo6}")
