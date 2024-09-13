ca3fc9f9e7891f1e42e564bb5bf2d1bea036b967
JohanRamirez1618 <johanramirez1618@gmail.com>
2024-09-13 16:25:59 -0500

    prueba

---
+++ edad.py
@@ -0,0 +1,15 @@
+años= int(input("D"))
+"""
+#Categorias
+if edad < 0:
+    print("Pertenece a categoria de Pre-Infancia")
+elif edad == 1 and edad >= 5:
+    print("Pertenece a categoria de Primera Infancia")
+elif edad == 6 and edad <= 11:
+    print("Pertenece a categoria de Infancia")
+elif edad == 12 and edad <= 18:
+    print("Pertenece a categoria de Adolecencia")
+elif edad == 19 and edad <= 59:
+    print("Pertenece a categoria de Adultez")
+elif edad >= 60:
+    print("Pertenece a categoria de Adulto Mayor")"""
